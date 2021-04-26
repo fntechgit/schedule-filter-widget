376ef5c44c5502cc3c9e6d68ed06995da2c23f7a
Tomás Castillo <tcastilloboireau@gmail.com>
2021-04-26 19:49:48 -0300

    fixing tag filters

--- src/components/filter-tags/index.js
+++ src/components/filter-tags/index.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from "./index.module.scss";
-import { Button } from 'react-bootstrap';
 
-const FilterTag = ({ options }) => {
+const FilterTag = ({ option, option: { tag, count, color }, onFilterChange, filtered }) => {
 
-    const [isActive, setIsActive] = useState(false);
+    const [isActive, setIsActive] = useState(null);    
+
+    useEffect(() => {
+        if (isActive === null && filtered[0]?.options.find(f => f === option)) {
+            setIsActive(true);
+        } else if (isActive !== null) {
+            isActive ? onFilterChange(option, true) : onFilterChange(option, false);
+        }
+    }, [isActive]);
 
     return (
         <button className={`${styles.tagButton} ${isActive ? styles.tagButtonActive : ''}`} onClick={() => setIsActive(!isActive)}>
-            <span className={styles.title}>Dark</span>
+            <span className={styles.title}>{tag}</span>
                 &nbsp;
-            <span className={styles.quantity}>(130)</span>
+            <span className={styles.quantity}>({count})</span>
         </button>
     )
 }
